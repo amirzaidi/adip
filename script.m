7107cdee8c4d96b9127dad53697271bbc4a72cbf
Willie Overman <w.c.a.overman@student.tudelft.nl>
2021-06-04 15:39:47 +0200

    Script file

---
+++ script.m
@@ -0,0 +1,28 @@
+% Progrmming assignment for AP3132-Advanced Digital Image Processing course
+% Instructor: B. Rieger, F. Vos 
+% Tutor: H. Heydarian
+% Term: Q3-2021
+%
+% Labwork #2
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Problem #1
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Part a: dilation and erosion
+
+% read the original input image
+I = imread('trui.tif');
+dipshow(mat2im(I));
+title('original image')
+
+% add proper noise to make the image the same as the one depicted in the 
+% manual (top-right in Figure 1)
+% TODO3
+J = noise(mat2im(I), 'gaussian', 10.0);
+dipshow(J)
+title('noisy image')
+
+
+F = anisotropic(mat2im(J), 0.25, 30, 40);
+dipshow(F)
+title('filtered image')
